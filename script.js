a0b3db9edca35c6e32ef3659d5ebff55fe8318bd
Egor <egor-syrov@bk.ru>
2022-04-04 11:44:41 +0700

    beta

--- script.js
+++ script.js
@@ -3,23 +3,80 @@ let y = 320 - 30;
 let dx = 2;
 let dy = -2;
 
+var ballRadius = 10;
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (480-paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
 
-function drawBall(){
-        ctx.beginPath();
-        ctx.arc(x, y, 10, 0, Math.PI*2);
-        ctx.fillStyle = "#0095DD";
-        ctx.fill();
-        ctx.closePath(); 
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
 }
 
-function draw(){
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawBall();
-        x += dx;
-        y += dy;
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
 }
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle();
+    if(x + dx > canvas.width || x + dx < 0) {
+        dx = -dx;
+    }
+    
+    if(y + dy > canvas.height || y + dy < 0) {
+        dy = -dy;
+    }
+
+    if(rightPressed) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if(leftPressed) {
+        paddleX -= 7;
+        if (paddleX < 0){
+            paddleX = 0;
+        }
+    }
+    
+    x += dx;
+    y += dy;
+}
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 setInterval(draw, 10);
-
-
